0f5108a7113a2da1165940fecd5cf456b9525125
David Flanagan <david@rawkode.academy>
2026-06-10 09:51:50 +0000

    bump: cuenv 0.53.0

--- Formula/cuenv.rb
+++ Formula/cuenv.rb
@@ -1,25 +1,25 @@
 class Cuenv < Formula
   desc "Modern application build toolchain with typed environments and CUE-powered task orchestration"
   homepage "https://github.com/cuenv/cuenv"
-  version "0.52.0"
+  version "0.53.0"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/cuenv/cuenv/releases/download/0.52.0/cuenv-darwin-arm64"
-      sha256 "034dea73eba01b198682a39d088a91a01d88c9e406d3a0b7d7bcd5c9f4134c0c"
+      url "https://github.com/cuenv/cuenv/releases/download/0.53.0/cuenv-darwin-arm64"
+      sha256 "7068fe7cbbfc76c7ef8f8118f43ab24519e913039cbbcc14520c7ba3d489e970"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/cuenv/cuenv/releases/download/0.52.0/cuenv-linux-x64"
-      sha256 "6ab780c16051b74d3768cf5d6dd575399a7046059c33feebbbf2939bebeb8278"
+      url "https://github.com/cuenv/cuenv/releases/download/0.53.0/cuenv-linux-x64"
+      sha256 "b22041f0a96c16ab4f0d1d200fcb0c35d194f22481a879b428d4a83d4370e57d"
     end
 
     on_arm do
-      url "https://github.com/cuenv/cuenv/releases/download/0.52.0/cuenv-linux-arm64"
-      sha256 "966fc52336e5f9a97a49b53e351b88fad77db21b694f1e7a9be3fbea3c7e335a"
+      url "https://github.com/cuenv/cuenv/releases/download/0.53.0/cuenv-linux-arm64"
+      sha256 "53af211fb5ff00f9cc1bf7b4ae7d20c8c08c755a3f6d82a7c8052f93911a1a24"
     end
   end
 
